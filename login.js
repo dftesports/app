f213a8ea2d1f4d2747aae1a8772cb09faa6e611b
dftesports <dftesports8@gmail.com>
2025-02-01 11:31:10 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+if (localStorage.getItem("dft_local")) {
+  window.location.href = "index.html";
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  var url = "https://script.google.com/macros/s/AKfycbwa_YWZH-RnH-xHCfstsVwcCN0t1dTGLYBpG_ysoykwOlAYNwU0rBzQDFjd8blYczM7/exec";
+
+  var data = {
+    "username": username,
+    "password": password
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(data)
+  })
+  .then(response => response.text())
+  .then(responseText => {
+    if (responseText === "success") {
+      localStorage.setItem("dft_local", username);
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password!");
+    }
+  })
+  .catch(error => console.error('Error:', error));
+});
